ff3d6a46fd75e1e2cce498bf166aeea65796490e
Parmilan <parmilancs2024@gmail.com>
2023-07-17 01:37:46 +0530

    Dialog flow chatbot implementation first commit

--- client/src/Screen/ContactUs.js
+++ client/src/Screen/ContactUs.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import ChatBot from '../Components/ChatBot'
 
 const ContactUs = () => {
   return (
     <div>
       <h2 className=' font-popins font-bold text-xl text-center'>ContactUs Page</h2>
-
+      <ChatBot/>
     </div>
   )
 }
