86f16c1f1f4d93a35e3952f381ce7acf9851b306
Parmilan <parmilancs2024@gmail.com>
2023-08-14 14:37:13 +0530

    Add Footwear UI style edit

--- client/src/screens/ProductCreateScreen.js
+++ client/src/screens/ProductCreateScreen.js
@@ -9,10 +9,7 @@ import FormContainer from '../components/FormContainer'
 import { createProduct } from '../actions/productActions'
 import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 import { date } from 'yup'
-import {
-  listCategories,
- 
-} from '../actions/categoryAction'
+import { listCategories } from '../actions/categoryAction'
 
 const ProductCreateScreen = () => {
   const { id: productId } = useParams()
@@ -81,8 +78,8 @@ const ProductCreateScreen = () => {
   }
 
   return (
-    <Container>
-      <>
+    <>
+      <Container className='pb-5'>
         <Link
           to='/admin/productlist'
           className='btn btn-light my-3'
@@ -96,207 +93,207 @@ const ProductCreateScreen = () => {
           Go Back
         </Link>
         <FormContainer>
-          <h1
-            className='tag my-5'
-            style={{
-              fontSize: '45px',
-              fontFamily: 'Lucida Console',
-              fontWeight: 'bold',
-            }}
-          >
-            Phonix
-          </h1>
-          {loadingCreate && <Loader />}
-          {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
-          {loading ? (
-            <Loader />
-          ) : error ? (
-            <Message variant='danger'>{error}</Message>
-          ) : (
-            <Form onSubmit={submitHandler} enctype='multipart/form-data'>
-              <Form.Group controlId='cat'>
-                <Form.Label
-                  className='mt-3'
+          <div className='py-3 px-3 bg-white rounded'>
+            <h1
+              className='text-center my-3'
+              style={{
+                fontSize: '28px',
+                fontWeight: 'bold',
+              }}
+            >
+              Add Footwear
+            </h1>
+            {loadingCreate && <Loader />}
+            {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
+            {loading ? (
+              <Loader />
+            ) : error ? (
+              <Message variant='danger'>{error}</Message>
+            ) : (
+              <Form onSubmit={submitHandler} enctype='multipart/form-data'>
+                <Form.Group controlId='cat'>
+                  <Form.Label
+                    className='mt-3'
+                    style={{
+                      fontSize: '18px',
+                      fontWeight: 'bold',
+                      color: '#591f1f',
+                    }}
+                  >
+                    Category
+                  </Form.Label>
+                  <Form.Control
+                    as='select'
+                    value={cat}
+                    onChange={(e) => setCategory(e.target.value)}
+                  >
+                    <option value=''>Select Category</option>
+                    {category.map((catItem) => (
+                      <option key={catItem._id} value={catItem.name}>
+                        {catItem.name}
+                      </option>
+                    ))}
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='gender'>
+                  <Form.Label
+                    className='mt-3'
+                    style={{
+                      fontSize: '18px',
+                      fontWeight: 'bold',
+                      color: '#591f1f',
+                    }}
+                  >
+                    Gender
+                  </Form.Label>
+                  <Form.Control
+                    as='select'
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
+                  >
+                    <option value=''>Select Gender</option>
+                    <option value='Male'>Male</option>
+                    <option value='Female'>Female</option>
+                    <option value='Unisex'>Unisex</option>
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='name'>
+                  <Form.Label
+                    className='mt-3'
+                    style={{
+                      fontSize: '18px',
+                      fontWeight: 'bold',
+                      color: '#591f1f',
+                    }}
+                  >
+                    Product
+                  </Form.Label>
+                  <Form.Control
+                    type='name'
+                    placeholder='Enter product'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='size'>
+                  <Form.Label
+                    className='mt-3'
+                    style={{
+                      fontSize: '18px',
+                      fontWeight: 'bold',
+                      color: '#591f1f',
+                    }}
+                  >
+                    Size
+                  </Form.Label>
+                  <Form.Control
+                    min={0}
+                    type='number'
+                    placeholder='Size'
+                    value={size}
+                    onChange={(e) => setSize(e.target.value)}
+                  ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='countInStock'>
+                  <Form.Label
+                    className='mt-3'
+                    style={{
+                      fontSize: '18px',
+                      fontWeight: 'bold',
+                      color: '#591f1f',
+                    }}
+                  >
+                    Count in Stock
+                  </Form.Label>
+                  <Form.Control
+                    min={0}
+                    type='number'
+                    placeholder='Count in Stock'
+                    value={countInStock}
+                    onChange={(e) => setCount(e.target.value)}
+                  ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='price'>
+                  <Form.Label
+                    className='mt-3'
+                    style={{
+                      fontSize: '18px',
+                      fontWeight: 'bold',
+                      color: '#591f1f',
+                    }}
+                  >
+                    Price
+                  </Form.Label>
+                  <Form.Control
+                    min={0}
+                    type='number'
+                    placeholder='Enter price'
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                  ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='image'>
+                  <Form.Label
+                    className='mt-3'
+                    style={{
+                      fontSize: '18px',
+                      fontWeight: 'bold',
+                      color: '#591f1f',
+                    }}
+                  >
+                    Image
+                  </Form.Label>
+                  <Form.Control
+                    id='image-file'
+                    type='file'
+                    custom
+                    onChange={uploadFileHandler}
+                  />
+                  {uploading && <Loader />}
+                </Form.Group>
+
+                <Form.Group controlId='description'>
+                  <Form.Label
+                    className='mt-3'
+                    style={{
+                      fontSize: '18px',
+                      fontWeight: 'bold',
+                      color: '#591f1f',
+                    }}
+                  >
+                    Description
+                  </Form.Label>
+                  <Form.Control
+                    type='text'
+                    placeholder='Enter description'
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  ></Form.Control>
+                </Form.Group>
+
+                <Button
+                  className='my-3'
+                  type='submit'
                   style={{
-                    fontSize: '18px',
-                    fontWeight: 'bold',
-                    color: '#591f1f',
+                    color: 'white',
+                    fontWeight: '600',
+                    borderRadius: '15px',
                   }}
                 >
-                  Category
-                </Form.Label>
-                <Form.Control
-                  as='select'
-                  value={cat}
-                  onChange={(e) => setCategory(e.target.value)}
-                >
-                   <option value=''>Select Category</option>
-                  {category.map((catItem) => (
-                    <option key={catItem._id} value={catItem.name}>
-                      {catItem.name}
-                    </option>
-                  ))}
-                </Form.Control>
-              </Form.Group>
-
-              <Form.Group controlId='gender'>
-                <Form.Label
-                  className='mt-3'
-                  style={{
-                    fontSize: '18px',
-                    fontWeight: 'bold',
-                    color: '#591f1f',
-                  }}
-                >
-                  Gender
-                </Form.Label>
-                <Form.Control
-                  as='select'
-                  value={gender}
-                  onChange={(e) => setGender(e.target.value)}
-                >
-                  <option value=''>Select Gender</option>
-                  <option value='Male'>Male</option>
-                  <option value='Female'>Female</option>
-                  <option value='Unisex'>Unisex</option>
-                </Form.Control>
-              </Form.Group>
-
-              <Form.Group controlId='name'>
-                <Form.Label
-                  className='mt-3'
-                  style={{
-                    fontSize: '18px',
-                    fontWeight: 'bold',
-                    color: '#591f1f',
-                  }}
-                >
-                  Product
-                </Form.Label>
-                <Form.Control
-                  type='name'
-                  placeholder='Enter product'
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                ></Form.Control>
-              </Form.Group>
-
-              <Form.Group controlId='size'>
-                <Form.Label
-                  className='mt-3'
-                  style={{
-                    fontSize: '18px',
-                    fontWeight: 'bold',
-                    color: '#591f1f',
-                  }}
-                >
-                  Size
-                </Form.Label>
-                <Form.Control
-                  min={0}
-                  type='number'
-                  placeholder='Size'
-                  value={size}
-                  onChange={(e) => setSize(e.target.value)}
-                ></Form.Control>
-              </Form.Group>
-
-              <Form.Group controlId='countInStock'>
-                <Form.Label
-                  className='mt-3'
-                  style={{
-                    fontSize: '18px',
-                    fontWeight: 'bold',
-                    color: '#591f1f',
-                  }}
-                >
-                  Count in Stock
-                </Form.Label>
-                <Form.Control
-                  min={0}
-                  type='number'
-                  placeholder='Count in Stock'
-                  value={countInStock}
-                  onChange={(e) => setCount(e.target.value)}
-                ></Form.Control>
-              </Form.Group>
-
-              <Form.Group controlId='price'>
-                <Form.Label
-                  className='mt-3'
-                  style={{
-                    fontSize: '18px',
-                    fontWeight: 'bold',
-                    color: '#591f1f',
-                  }}
-                >
-                  Price
-                </Form.Label>
-                <Form.Control
-                  min={0}
-                  type='number'
-                  placeholder='Enter price'
-                  value={price}
-                  onChange={(e) => setPrice(e.target.value)}
-                ></Form.Control>
-              </Form.Group>
-
-              <Form.Group controlId='image'>
-                <Form.Label
-                  className='mt-3'
-                  style={{
-                    fontSize: '18px',
-                    fontWeight: 'bold',
-                    color: '#591f1f',
-                  }}
-                >
-                  Image
-                </Form.Label>
-                <Form.Control
-                  id='image-file'
-                  type='file'
-                  custom
-                  onChange={uploadFileHandler}
-                />
-                {uploading && <Loader />}
-              </Form.Group>
-
-              <Form.Group controlId='description'>
-                <Form.Label
-                  className='mt-3'
-                  style={{
-                    fontSize: '18px',
-                    fontWeight: 'bold',
-                    color: '#591f1f',
-                  }}
-                >
-                  Description
-                </Form.Label>
-                <Form.Control
-                  type='text'
-                  placeholder='Enter description'
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                ></Form.Control>
-              </Form.Group>
-
-              <Button
-                type='submit'
-                style={{
-                  backgroundImage:
-                    'linear-gradient(to bottom right,#79db58,#036920,#79db58)',
-                  color: 'white',
-                  fontWeight: '600',
-                  borderRadius: '15px',
-                }}
-              >
-                Create
-              </Button>
-            </Form>
-          )}
+                  Create
+                </Button>
+              </Form>
+            )}
+          </div>
         </FormContainer>
-      </>
-    </Container>
+      </Container>
+    </>
   )
 }
 
