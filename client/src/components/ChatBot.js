c0b1b48845bf53c4c34032ded22f3f4b1fdf1d4c
Parmilan <parmilancs2024@gmail.com>
2023-12-17 22:52:44 +0530

    kommunicate API change

--- client/src/components/ChatBot.js
+++ client/src/components/ChatBot.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react'
-import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
+import Kommunicate from '@kommunicate/kommunicate-chatbot-plugin'
 
-Kommunicate.init("3965db72f34386a490631d4c0c9e61eb9", {
-    automaticChatOpenOnNavigation: true,
-    popupWidget: true
-  });
+Kommunicate.init('1e2c79ba92a9bfdcc1881c5bd3e731634', {
+  automaticChatOpenOnNavigation: true,
+  popupWidget: true,
+})
 
 export default class ChatBot extends Component {
   render() {
-    return (
-      <>
-        
-      </>
-    )
+    return <></>
   }
 }
